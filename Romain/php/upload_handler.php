1e1c2cc1dd91e20e74453528e7db660a5342e6ed
RValeye <romain.valeye@gmail.com>
2016-09-15 17:26:08 +0200

    chemin rel/absolu

--- Romain/php/upload_handler.php
+++ Romain/php/upload_handler.php
@@ -1,22 +1,25 @@
 <?php
 require_once "fonctions-bdd.php";
-define("UPLOAD_DIR", "/uploads/");
+define("UPLOAD_DIR", "../uploads/");
 
 if (!empty($_FILES["myFile"])) {
     $myFile = $_FILES["myFile"];
 
     switch ($_POST['pu_type_fichier']) {
         case '1':
-            $upload_direction = UPLOAD_DIR . "image/";
+            $upload_rel = UPLOAD_DIR . "image/";
+            $upload_direction = "/uploads/image/";
             break;
         case '2':
-            $upload_direction = UPLOAD_DIR . "video/";
+            $upload_rel = UPLOAD_DIR . "video/";
+            $upload_direction = "/uploads/video/";
             break;
         case '3':
-            $upload_direction = UPLOAD_DIR . "musique/";
+            $upload_rel = UPLOAD_DIR . "musique/";
+            $upload_direction = "/uploads/musique/";
             break;
         default:
-            $upload_direction = "";
+            $upload_rel = "";
             break;
     }
     if ($myFile["error"] !== UPLOAD_ERR_OK) {
@@ -29,14 +32,14 @@ if (!empty($_FILES["myFile"])) {
     // don't overwrite an existing file
     $i = 0;
     $parts = pathinfo($name);
-    while (file_exists($upload_direction . $name)) {
+    while (file_exists($upload_rel . $name)) {
         $i++;
         $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
     }
 
     // preserve file from temporary directory
     $success = move_uploaded_file($myFile["tmp_name"],
-        $upload_direction . $name);
+        $upload_rel . $name);
     if (!$success) {
         die("Unable to save file.");
     }
